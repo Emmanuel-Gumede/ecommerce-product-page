288591fdc2231bafaee549c34e3f6cd20b3ca38b
Emmanuel Gumede <gumede.em@gmail.com>
2023-04-22 04:21:50 +0200

    update readme file

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,12 @@
 const Modal = (props) => {
-  return <div className={`page__modal ${props.hide ? 'page__modal-hide' : ''}`}></div>;
+  return (
+    <div
+      className={`page__modal ${props.hide ? 'page__modal-hide' : ''}`}
+      onClick={() => {
+        console.log('modal clicked');
+      }}
+    ></div>
+  );
 };
 
 export default Modal;
